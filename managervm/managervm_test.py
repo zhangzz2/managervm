33b40326261cce2f37e38bdc39ccea8cd7d24cb5
zhangjf <zhangjunfeng@huayunwangji.com>
2015-10-30 11:40:45 +0800

    add setup

---
+++ managervm/managervm_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+#-*- coding: utf-8 -*-
+
+def test_admin():
+    pass
+
+def test_ctl():
+    pass
+
+def test_guest():
+    pass
+
+if __name__ == "__main__":
+    print "hello, word!"
